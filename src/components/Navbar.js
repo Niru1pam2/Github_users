fa1c29b7afbcdbf689f1568c16001c048b8add84
Niru1pam2 <nirupamtalukdar123@gmail.com>
2024-08-15 10:51:47 +0530

    added authentication

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import styled from "styled-components";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
-  return <Wrapper>navbar component</Wrapper>;
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
+    useAuth0();
+  console.log(isAuthenticated, user);
+
+  const isUser = isAuthenticated && user;
+
+  return (
+    <Wrapper>
+      {isUser && user.picture && <img src={user.picture} alt="user" />}
+      {isUser && user.name && (
+        <h4>
+          Welcome, <strong>{user.name.toUpperCase()}</strong>
+        </h4>
+      )}
+      {isUser ? (
+        <button
+          onClick={() => {
+            logout({ returnTo: window.location.origin });
+          }}
+        >
+          logout
+        </button>
+      ) : (
+        <button onClick={loginWithRedirect}>login</button>
+      )}
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.nav`
--- src/index.js
+++ src/index.js
@@ -6,11 +6,21 @@ import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//domain dev-pytl84pka6wl11c3.us.auth0.com
+//clietn id 1IpaJfbn1QZhWQsDagYYaAiOi2va1UIe
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-pytl84pka6wl11c3.us.auth0.com"
+      clientId="1IpaJfbn1QZhWQsDagYYaAiOi2va1UIe"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
